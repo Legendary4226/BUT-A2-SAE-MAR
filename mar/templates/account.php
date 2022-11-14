6ef6a3c3aec9dff9ae427516568b6788ec771eb9
LAVIEILLE Alexis <alexis.lavieille@etu.univ-lyon1.fr>
2022-11-14 15:51:53 +0100

    Work: CSS all pages

--- mar/templates/account.php
+++ mar/templates/account.php
@@ -9,6 +9,7 @@
     <?php require_once(TEMPLATES . "ressources/css_files.php") ?>
 
     <!-- Specific CSS Files -->
+    <link rel="stylesheet" href="<?= STYLES ?>templates/account.css">
 </head>
 <body>
     <?php require_once("ressources/header.php"); ?>
@@ -16,25 +17,36 @@
     <main>
         
         <div>
-            <button>Profile and Settings</button>
+            <button class="button-selected">Profile and Settings</button>
             <button>Share my spaces</button>
         </div>
         <div>
             <div>
-                <form mehode="get" action="">
-                    <label for="email" > Email:</label>
-                    <input id="email" type="email" name="email">
-                    
-                    <label for="pseudo"> Pseudo:</label>
-                    <input id="pseudo" type="text" name="pseudo">
-
-                    <label for="changepass"> Change Pass: </label>
-                    <input id="changepass" type="password" name="changepass">
-
-                    <label for="Theme"> Confirm Pass: </label>
-                    <input id="confpass" type="password" name="confpass">
-
-                    <input type="submit" value="Sign in">
+                <form methode="get" action="">
+                    <span>
+                        <label for="pseudo"> Pseudo:</label>
+                        <input id="pseudo" type="text" name="Actual Pseudo" placeholder="Pseudo">
+                    </span>
+                    <span>
+                        <label for="email" > Email:</label>
+                        <input id="email" type="email" name="email" placeholder="Actual Email">
+                    </span>
+                    <span>
+                        <label for="changepass"> Change Pass: </label>
+                        <input id="changepass" type="password" name="changepass" placeholder="Enter new pass">
+                    </span>
+                    <span>
+                        <label for="confpass"> Confirm Pass: </label>
+                        <input id="confpass" type="password" name="confpass" placeholder="Confirm new pass">
+                    </span>
+                    <span>
+                        <label for="theme"> Theme: </label>
+                        <select name="theme" id="theme">
+                            <option>Default-White</option>
+                            <option>Dark</option>
+                        </select>
+                    </span>
+                    <input type="submit" value="submit" class="save">
                 </form>
             </div>
             <div>
