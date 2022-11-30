f6d2df3ce566076648ffea0749218d61350cb3b8
nameko38 <94040730+nameko38@users.noreply.github.com>
2022-11-30 14:33:34 +0100

    texte page d'accueil

--- mar/templates/home.php
+++ mar/templates/home.php
@@ -22,7 +22,10 @@ $headerButtonsLinks = array(
 
 <main>
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Bienvenue sur notre site My Amazing Reminder. Il vous permettra de stocker toutes vos tâches du quotidien sous 
+        la forme de listes. L'interface simple et épurée donne une vision optimale sur les différents éléments remplis 
+        précédemment. Parmi les fonctionnalités disponibles, vous pourrez également retrouver le partage de boîtes ou 
+        les espaces collaboratifs. 
     </p>
     
     <img src="">
