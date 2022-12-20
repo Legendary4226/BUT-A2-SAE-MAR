67fb6dc2edadbb2a15c072a7fa5bd1ea81fa6221
Richard G <richard.guilland@etu.univ-lyon1.fr>
2022-12-20 23:25:52 +0100

    Refactor: cleanup JS + tweaks on HTML & CSS

--- mar/templates/space.php
+++ mar/templates/space.php
@@ -12,81 +12,94 @@
     <link rel="stylesheet" href="<?= STYLES ?>templates/space.css">
 </head>
 <body>
-    <?php require_once("ressources/header.php"); ?>
+<?php require_once("ressources/header.php"); ?>
 
-    <main>
-        <section class="panel">
-            <span>
-                <i></i>
-                <button>Profile and Settings</button>
-            </span>
-            <span>
-                <i></i>
-                <button class="button-selected">Share my spaces</button>
-            </span>
+<main>
+    <section class="panel">
+        <span>
+            <i></i>
+            <button>Profile and Settings</button>
+        </span>
+        <span>
+            <i></i>
+            <button class="button-selected">Share my spaces</button>
+        </span>
 
-        </section>
+    </section>
 
-        <section class="panel secondary">
-            <span>
-                <i></i>
-                <button class="button-selected">One of my Spaces</button>
-            </span>
-            <span>
-                <i></i>
-                <button>Another Space</button>
-            </span>
-            
-            
-        </section>
+    <section class="panel secondary">
+        <span>
+            <i></i>
+            <button class="button-selected">One of my Spaces</button>
+        </span>
+        <span>
+            <i></i>
+            <button>Another Space</button>
+        </span>
         
-        <section>
-            <form methode="POST" action="" class="form-edit-space-settings">
-                <fieldset class="space-name">
-                    <label for="space-name">Space Name :</label>
-                    <input id="space-name" type="text" name="space-name" value="One of my Spaces" disabled> 
-                </fieldset>
+        
+    </section>
+    
+    <section>
+        <form methode="POST" action="" class="form-edit-space-settings">
+            <fieldset class="space-name">
+                <label for="space-name">Space Name :</label>
+                <input id="space-name" type="text" name="space-name" value="One of my Spaces" maxlength="35" required> 
+            </fieldset>
 
-                <h1>Share with:</h1>
+            <h1>Share with:</h1>
 
-                <fieldset class="space-share">
-                    
+            <fieldset class="space-share">
 
-                    <span id="init-clone" class="user">
-                        <input type="text" name="email" placeholder="example@email.com">
-                        <label for="permission"> Permission :</label>
-                        <select name="permission" id="permission">
-                            <option>Read</option>
-                            <option>Edit</option>
-                        </select>
+            <span>
+                    <input type="email" name="520" placeholder="example@email.com" required>
+                    <label for="ID:permission"> Permission :</label>
+                    <select name="ID:permission" id="ID:permission">
+                        <option value="read">Read</option>
+                        <option value="edit">Edit</option>
+                    </select>
 
-                        <button class="action-button transition-simple-bump" id="delete-button-example" class="delete-button" type="button">
-                            <?php require(ICON_SVG_CLOSE) ?>
+                    <button class="action-button transition-simple-bump" class="delete-button" type="button">
+                        <?php require(ICON_SVG_CLOSE) ?>
+                    </button>
+                </span>
+
+                <span id="init-clone">
+                    <input type="email" name="ID" placeholder="example@email.com" required>
+                    <label for="ID:permission"> Permission :</label>
+                    <select name="ID:permission" id="ID:permission">
+                        <option value="read">Read</option>
+                        <option value="edit">Edit</option>
+                    </select>
+
+                    <button class="action-button transition-simple-bump" class="delete-button" type="button">
+                        <?php require(ICON_SVG_CLOSE) ?>
+                    </button>
+                </span>
+                
+            </fieldset>
+
+            <fieldset>
+                <button class="action-button transition-simple-bump" id="save-modifications">
+                    <?php require(ICON_SVG_SAVE) ?>
+                </button>
+            </fieldset>
+        </form>
+
+        <form id="add-user-form">
+            <fieldset class="space-share">
+                    <span>
+                        <input class="input-add-people-email" type="email" id="new-person" placeholder="example@email.com" maxlength="35">
+                        <button class="action-button transition-simple-bump">
+                            <?php require_once(ICON_SVG_PLUS) ?>
                         </button>
                     </span>
-                </fieldset>
+            </fieldset>
+        </form>
+    </section>
+</main>
 
-                <fieldset>
-                    <button class="action-button transition-simple-bump" id="save-modifications">
-                        <?php require(ICON_SVG_SAVE) ?>
-                    </button>
-                </fieldset>
-            </form>
-
-            <form id="add-user-form">
-                <fieldset class="space-share">
-                        <span>
-                            <input class="input-add-people-email" type="text" id="new-person" placeholder="Add a new person">
-                            <button class="action-button transition-simple-bump" id="add-shared-people">
-                                <?php require_once(ICON_SVG_PLUS) ?>
-                            </button>
-                        </span>
-                </fieldset>
-            </form>
-        </section>
-    </main>
-
-    <?php require_once("ressources/footer.php"); ?>
-    <script src="<?= TEMPLATES ?>ressources/JS/space.js"></script>
+<?php require_once("ressources/footer.php"); ?>
+<script src="<?= TEMPLATES ?>ressources/JS/space.js"></script>
 </body>
 </html>
